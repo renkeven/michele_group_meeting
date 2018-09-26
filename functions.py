f8e2f829d520270046f2e53501278ef03a1337e3
renkeven <kevenr@student.unimelb.edu.au>
2018-09-26 13:06:55 +1000

    Friday Meetings

--- functions.py
+++ functions.py
@@ -11,7 +11,12 @@ def load_schedule(sdate,topic,presenter):
 
 def update_schedule(schedule):
     
+    Period_Dictionary = {0:'W-Mon', 1:'W-Tue', 2:'W-Wed', 3:'W-Thu', 4:'W-Fri'}
+    Current_Day = schedule['Date'].dt.dayofweek[0]
+
     Start_Schedule_Date = schedule.iloc[0,schedule.columns.get_loc('Date')]
+    
+    
     
     if Start_Schedule_Date > pd.Timestamp(pd.datetime.now().date()):        
         return schedule
@@ -23,7 +28,7 @@ def update_schedule(schedule):
         New_Schedule = schedule[schedule['Date'] >= pd.Timestamp(pd.datetime.now().date())].append(schedule[schedule['Date'] < pd.Timestamp(pd.datetime.now().date())])
         New_Schedule = New_Schedule.reset_index(drop=True)
     
-        New_Dates = pd.date_range(End_Schedule_Date + pd.DateOffset(days=1), periods = Start_Today_Periods, freq='W-Thu')
+        New_Dates = pd.date_range(End_Schedule_Date + pd.DateOffset(days=1), periods = Start_Today_Periods, freq=Period_Dictionary[Current_Day])
         New_Schedule.iloc[-Start_Today_Periods:, New_Schedule.columns.get_loc('Date')] = New_Dates
     
         return New_Schedule
@@ -44,8 +49,25 @@ def update_schedule(schedule):
 
 def Offset_Schedule(schedule,date,weeks_num):
     #Date Offset by 1 week at start_date
-    Insert_Index = schedule[schedule['Date'] >= date].index[0]
-    Offset = schedule.iloc[Insert_Index:,schedule.columns.get_loc('Date')] + pd.DateOffset(weeks=weeks_num)
-    schedule.iloc[Insert_Index:,schedule.columns.get_loc('Date')] = Offset
+    
+    if (schedule['Date'] == date).any() == True:
+    
+        Insert_Index = schedule[schedule['Date'] >= date].index[0]
+        Offset = schedule.iloc[Insert_Index:,schedule.columns.get_loc('Date')] + pd.DateOffset(weeks=weeks_num)
+        schedule.iloc[Insert_Index:,schedule.columns.get_loc('Date')] = Offset
+    
+    else:
+        print 'Date not present in dataframe'
+    
+    return schedule
+
+def DoW_Schedule(schedule,proposed_day):
+    #Change Meeting Start Dates
+    #0 = Monday, 4 = Friday
+    
+    current_day = schedule['Date'].dt.dayofweek[0]
+    Offset = schedule['Date'] + pd.DateOffset(days=(proposed_day - current_day))        
+    
+    schedule['Date'] = Offset
     
     return schedule
