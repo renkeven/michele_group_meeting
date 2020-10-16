973e3416db20f57e9124a9772371289e9378ff5b
keven ren <ren.keven@gmail.com>
2020-10-16 19:27:39 +1100

    clashes + completion talk

--- functions.py
+++ functions.py
@@ -40,11 +40,11 @@ def update_schedule(schedule):
         New_Schedule = schedule[schedule['Date'] >= pd.Timestamp(pd.datetime.now().date())].append(schedule[schedule['Date'] < pd.Timestamp(pd.datetime.now().date())])
         New_Schedule = New_Schedule.reset_index(drop=True)
 
-        print(New_Schedule)
+        #print(New_Schedule)
         
         New_Dates = pd.date_range(End_Schedule_Date + pd.DateOffset(days=1), periods = Start_Today_Periods, freq=Period_Dictionary[Current_Day])
         
-        print(New_Dates)
+        #print(New_Dates)
         
         New_Schedule.iloc[-Start_Today_Periods:, New_Schedule.columns.get_loc('Date')] = New_Dates
     
@@ -66,7 +66,7 @@ def update_schedule(schedule):
 
 def Offset_Schedule(schedule,date,weeks_num, special_offset=False):
     #Date Offset by 1 week at start_date
-    #Special Offset shifts the whole week starting a day, instead of 'the day'
+    #Special Offset shifts the whole week starting a day, instead of 'the day'        
     
     if (special_offset == False):
         if (schedule['Date'] == date).any() == True:
@@ -86,6 +86,29 @@ def Offset_Schedule(schedule,date,weeks_num, special_offset=False):
     
     return schedule
 
+def MoveSpeaker(schedule, olddate, newdate):
+    #if olddate > newdate, then all speakers slide down 1 week
+    #if newdate > olddate, then all speakers slide up 1 week
+    
+    assert (schedule['Date'] == olddate).any() == True
+    assert (schedule['Date'] == newdate).any() == True
+    assert (olddate != newdate)
+    
+    idx_old = schedule[schedule['Date'] == olddate].index[0]
+    idx_new = schedule[schedule['Date'] == newdate].index[0]
+        
+    name_range = schedule.iloc[idx_old:idx_new+1,schedule.columns.get_loc('Speaker')]
+    
+    if olddate < newdate:
+        name_range = np.roll(name_range,-1)
+                
+    if newdate < olddate:
+        name_range = np.roll(name_range, 1)
+    
+    schedule.iloc[idx_old:idx_new+1,schedule.columns.get_loc('Speaker')] = name_range
+
+    return schedule
+        
 def Add_Special_Replacement(schedule,date,speaker,topic, offset=True):
     #Usual Meeting REPLACED or Special Meeting ADDED (Determined by Offset parameter)
     
@@ -100,21 +123,32 @@ def Add_Special_Replacement(schedule,date,speaker,topic, offset=True):
 
     return schedule
 
-def Mask_Date(schedule,date,date_move,T_P=1):
+def Mask_Date(schedule,date,date_move,T_P=1, preserve_order=False):
     #T_P is Tie-Priority. Only need if we have speakers in a row to miss talks.
     #Artifically add a small buffer time and mask it.
+    #save all speaking index past date
 
+    topic_range = schedule.iloc[schedule[(schedule['Date']==date)].index[0]:,schedule.columns.get_loc('Topic')]
+    
     date_move = pd.Timestamp(date_move) + pd.DateOffset(hours=T_P)    
     schedule.iloc[schedule[(schedule['Date']==date)].index[0],schedule.columns.get_loc('Date')] = date_move
     
     if date_move > pd.Timestamp(pd.datetime.now().date()):        
         schedule = schedule[~(schedule['Date'] == date_move)]
         schedule = schedule.reset_index(drop=True)
+        
+        if preserve_order:
+            schedule.iloc[schedule[(schedule['Date'] > date)].index[0]:,schedule.columns.get_loc('Topic')] = topic_range[:-1]
+        
         return schedule
     
     else:
         schedule = schedule.sort_values('Date')  #exp
         schedule = schedule.reset_index(drop=True)
+        
+        if preserve_order:
+            schedule.iloc[schedule[(schedule['Date'] > date)].index[0]:,schedule.columns.get_loc('Topic')] = topic_range[:-1]
+        
         return schedule
     
 
